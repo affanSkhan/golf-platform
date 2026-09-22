a542a2e85fc24cfd921c8bbffb9cbd25616f3fea
Affan Khan <khanaffan070@gmail.com>
2026-09-22 17:38:58 +0530

    security: revoke public function execution

---
+++ supabase/migrations/0004_revoke_public_function_execute.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.handle_new_user() from public;
+revoke execute on function public.is_admin() from public;
+revoke execute on function public.trim_golf_scores() from public;
